7b53379bfe9870a84113f36a6fb808b860349d6a
Meowcino <meow@catboy.au>
2024-04-16 20:31:05 +1000

    feature: convert tif to jpg

--- text_extraction/generate_thumbnails.py
+++ text_extraction/generate_thumbnails.py
@@ -1,27 +1,39 @@
 # This python script generates web compatible thumbnails (JPEG) from
-# TGA images provided by the Trendall Centre.
+# TIF images provided by the Trendall Centre.
+import os
+
 from PIL import Image
+from pathlib import Path
 
 # GLOBALS
 # Set compression quality.
-JPG_QUALITY = 100
+JPG_QUALITY = 75
 
-TGA_PATH = "./TGA/"  # Where the TGAs are.
+TIF_PATH = "./TIF/"  # Where the TIFs are.
 JPG_PATH = "./JPG/"  # Where you want the JPGs saved.
 
 
-def generate_thumbnails(record_id, ):
+def generate_thumbnail(tif_file):
     # Some records might not contain an image.
     try:
-        tga = Image.open(tga_path)
+        tif = Image.open(tif_file)
     except FileNotFoundError:
-        print(f"[!] No TGA File at {tga_path}, skipping.")
+        print(f"[!] No TIF File at {TIF_PATH}, skipping.")
         return
     except OSError:
-        print(f"[!] Error: Could not open the TGA file.")
+        print(f"[!] Error: Could not open the TIF file.")
         return
 
+    # Retain filename for JPG output.
+    jpg_out = JPG_PATH + Path(os.path.basename(tif_file)).stem + ".jpeg"
+
     # Correct the color mode if it isn't
-    if tga.mode != "RGB":
-        tga = tga.convert("RGB")
+    if tif.mode != "RGB":
+        tif = tif.convert("RGB")
+
+    tif.save(jpg_out, quality=JPG_QUALITY, format="JPEG", optimize=True)
+
+
+if __name__ == "__main__":
+    generate_thumbnail("./TIF/P-9-2.tif")
 
