9c19deb479ced60e6f225c6ea013446e7be12507
Meowcino <meow@catboy.au>
2024-04-18 22:08:19 +1000

    feature: rudimentary auth using next-auth and amazon cognito.

--- src/app/user/loading.jsx
+++
@@ -1,5 +0,0 @@
-export default function Page() {
-    return (
-        <h1 className={"text-4xl font-bold"}>Authenticating...</h1>
-    )
-}
--- src/app/user/page.jsx
+++ src/app/user/page.jsx
@@ -1,7 +1,11 @@
-export default function Page() {
+import {options} from "@/app/api/auth/[...nextauth]/options";
+import { getServerSession } from "next-auth";
+
+export default async function UserPage({}) {
+    const session = await getServerSession(options);
     return (
         <div className={"m-4"}>
-            <h1 className={"text-4xl font-bold"}>Your Account</h1>
+            <h1 className={"text-4xl font-bold"}>Hello {session.user.email}</h1>
             <p>This is the user page. You can only see this if you're logged in.</p>
             <p>If you aren't logged in you are supposed to see a login page instead.</p>
         </div>
