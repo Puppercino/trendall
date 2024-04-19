73da1085375170208ae79e0e95d5dc7a75f4ec1a
Meowcino <meow@catboy.au>
2024-04-20 01:39:06 +1000

    Add 404 page.

---
+++ src/app/not-found.jsx
@@ -0,0 +1,14 @@
+export default function notFound() {
+    return (
+        <>
+            <h1>
+                <i className="opacity-50">404</i><br></br>
+                Not Found
+            </h1>
+            <p>
+                Looks like there is nothing here. <br></br>
+                If there was, it has been either moved, deleted or hidden.
+            </p>
+        </>
+    )
+}
