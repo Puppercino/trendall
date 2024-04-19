92f9eea5eda7f0e1435276024e8fc4ddcfe9f317
Meowcino <meow@catboy.au>
2024-04-20 01:38:01 +1000

    User Pages and Components

--- src/app/user/page.jsx
+++ src/app/user/page.jsx
@@ -1,13 +1,14 @@
 import {options} from "@/app/api/auth/[...nextauth]/options";
 import { getServerSession } from "next-auth";
+import {UserProfile} from "@/app/user/components/UserProfile";
+import {UserMenu} from "@/app/user/components/UserMenu";
 
 export default async function UserPage({}) {
     const session = await getServerSession(options);
     return (
-        <div className={"m-4"}>
-            <h1 className={"text-4xl font-bold"}>Hello {session.user.email}</h1>
-            <p>This is the user page. You can only see this if you're logged in.</p>
-            <p>If you aren't logged in you are supposed to see a login page instead.</p>
-        </div>
+        <>
+            <UserMenu></UserMenu>
+            <UserProfile></UserProfile>
+        </>
     )
 }
