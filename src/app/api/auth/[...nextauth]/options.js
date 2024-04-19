c0156bddcdbb47cc45dced1fa95e056c7e4a31ab
Meowcino <meow@catboy.au>
2024-04-20 01:37:07 +1000

    Remove trailing comma

--- src/app/api/auth/[...nextauth]/options.js
+++ src/app/api/auth/[...nextauth]/options.js
@@ -8,7 +8,7 @@ export const options = {
         CognitoProvider({
             clientId: process.env.COGNITO_CLIENT_ID,
             clientSecret: process.env.COGNITO_CLIENT_SECRET,
-            issuer: process.env.COGNITO_ISSUER,
+            issuer: process.env.COGNITO_ISSUER
         })
     ],
     secret: process.env.NEXTAUTH_SECRET,
