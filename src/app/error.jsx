c49166ff221d0705df34c7c811aa6ba9f07e8a43
Meowcino <meow@catboy.au>
2024-04-20 19:05:29 +1000

    Add basic catch all error page.

---
+++ src/app/error.jsx
@@ -0,0 +1,15 @@
+"use client";
+
+export default function ErrorPage() {
+    return (
+        <>
+            <h1>
+                <i className="opacity-50">Ouch!</i><br></br>
+                Something went wrong.
+            </h1>
+            <p>
+                Whoops.
+            </p>
+        </>
+    )
+}
