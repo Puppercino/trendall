103721be09addd9615451479f5c04822ae0e79c1
Meowcino <meow@catboy.au>
2024-04-20 19:05:15 +1000

    Make navbar structure clearer.

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,12 +1,17 @@
+"use client";
 import {NavLink} from "@/app/components/NavLink";
 
 export function Navbar() {
     return (
-        <nav className="border-stone-150 flex justify-center gap-5 border-b bg-stone-100 p-5 text-stone-950">
-            <h1 className="grow p-1 text-2xl font-bold text-stone-950">Trendall</h1>
-            <NavLink href="/">Home</NavLink>
-            <NavLink href="/search">Search</NavLink>
-            <NavLink href="/user">Account</NavLink>
+        <nav className="border-stone-150 flex justify-between border-b bg-stone-100 text-stone-950">
+            <div className="p-6">
+                <h1 className="grow text-2xl font-bold text-stone-950">Trendall Project</h1>
+            </div>
+            <div className="flex justify-center gap-5 p-5">
+                <NavLink href="/">Home</NavLink>
+                <NavLink href="/search">Search</NavLink>
+                <NavLink href="/user">Account</NavLink>
+            </div>
         </nav>
     )
 }
