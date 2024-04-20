0fc8808359e67c5212eba91c6cce0f285c59506d
Meowcino <meow@catboy.au>
2024-04-20 18:36:42 +1000

    Move record controller and model into Next API.

---
+++ src/app/api/db/model/record_model.js
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose';
+
+// Record Schema for MongoDB, originally written by Vinh in Express.
+const recordSchema = mongoose.Schema({
+    ref_no: {
+        type: Number,
+        required: true
+    },
+    shape: {
+        type: String,
+        required: true
+    },
+    curr_coll: {
+        type: String,
+        required: false
+    },
+    prev_coll: {
+        type: String,
+        required: false
+    },
+    provenance: {
+        type: String,
+        required: false
+    },
+    height: {
+        type: String,
+        required: false
+    },
+    diameter: {
+        type: String,
+        required: false
+    },
+    plate: {
+        type: String,
+        required: false
+    },
+    publication: {
+        type: String,
+        required: false
+    },
+    description: {
+        type: String,
+        required: false
+    },
+}, { timestamps: true });
+
+const recordModel = mongoose.model("Record", recordSchema);
+
+export { recordModel as recordModel };
