6a14fff2b3fdd091921c4b16ae0879cac35dd1fe
Meowcino <meow@catboy.au>
2024-04-20 21:30:53 +1000

    move: user components into app components

--- src/app/user/page.jsx
+++ src/app/user/page.jsx
@@ -1,10 +1,7 @@
-import {options} from "@/app/api/auth/[...nextauth]/options";
-import { getServerSession } from "next-auth";
-import {UserProfile} from "@/app/user/components/UserProfile";
-import {UserMenu} from "@/app/user/components/UserMenu";
+import {UserProfile} from "@/app/components/UserProfile";
+import {UserMenu} from "@/app/components/UserMenu";
 
 export default async function UserPage({}) {
-    const session = await getServerSession(options);
     return (
         <>
             <UserMenu></UserMenu>
