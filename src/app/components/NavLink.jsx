d0dc863128e3d617c35121fb42b8ab953ec9780d
Meowcino <meow@catboy.au>
2024-04-21 22:24:06 +1000

    feature: current page highlights in navbar

--- src/app/components/NavLink.jsx
+++ src/app/components/NavLink.jsx
@@ -1,9 +1,18 @@
 "use client";
 import Link from "next/link";
 
-export function NavLink({ href, children }) {
+export function NavLink({ href, active, children }) {
+
+    let linkStateClasses;
+
+    // Inverts the button so that the end user knows where they are in the navbar.
+    if (active) {
+        linkStateClasses = "rounded px-4 py-2 text-stone-50 bg-stone-800";
+    } else {
+        linkStateClasses = "rounded px-4 py-2 text-stone-950 transition hover:bg-stone-800 hover:text-stone-50"
+    }
     return (
-        <Link href={href} className="rounded px-4 py-2 text-stone-950 transition hover:bg-stone-800 hover:text-stone-50">
+        <Link href={href} className={linkStateClasses}>
             {children}
         </Link>
     )
