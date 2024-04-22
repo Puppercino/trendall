28f93757416b0250de2e217fc8043390ac9bda32
Meowcino <meow@catboy.au>
2024-04-22 17:27:51 +1000

    feature: add queue that automatically detects tifs in TIF folder.

--- text_extraction/generate_thumbnails.py
+++ text_extraction/generate_thumbnails.py
@@ -1,16 +1,49 @@
 # This python script generates web compatible thumbnails (JPEG) from
 # TIF images provided by the Trendall Centre.
 import os
+import glob
 
 from PIL import Image
 from pathlib import Path
 
 # GLOBALS
 # Set compression quality.
-JPG_QUALITY = 50
+JPG_QUALITY = 100
 
 TIF_PATH = "./TIF/"  # Where the TIFs are.
 JPG_PATH = "./JPG/"  # Where you want the JPGs saved.
+
+
+def get_tifs(directory):
+    return glob.glob(f'{directory}/*.tif')
+
+
+# aspect_crop: crops the image to a square for thumbnail.
+def aspect_crop(image):
+    # Get the aspect ratio of the image.
+    aspect = image.width / image.height
+
+    # If the image is wider than it is tall, crop the sides.
+    if aspect > 1:
+        new_width = image.height
+        left = (image.width - new_width) / 2
+        right = (image.width + new_width) / 2
+        top = 0
+        bottom = image.height
+        return image.crop((left, top, right, bottom))
+
+    # If the image is taller than it is wide, crop the top and bottom.
+    elif aspect < 1:
+        new_height = image.width
+        left = 0
+        right = image.width
+        top = (image.height - new_height) / 2
+        bottom = (image.height + new_height) / 2
+        return image.crop((left, top, right, bottom))
+
+    # If the image is square, return the image.
+    else:
+        return image
 
 
 def generate_thumbnail(tif_file):
@@ -31,9 +64,14 @@ def generate_thumbnail(tif_file):
     if tif.mode != "RGB":
         tif = tif.convert("RGB")
 
+    tif = aspect_crop(tif)
+    tif = tif.resize((150, 150))
+
     tif.save(jpg_out, quality=JPG_QUALITY, format="JPEG", optimize=True)
 
 
 if __name__ == "__main__":
-    generate_thumbnail("./TIF/P-9-2.tif")
+    queue = get_tifs(TIF_PATH)
+    for original in queue:
+        generate_thumbnail(original)
 
