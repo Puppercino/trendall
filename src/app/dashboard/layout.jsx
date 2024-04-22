184fd88ea9a284c66becf039638eb9cd591e8998
Meowcino <meow@catboy.au>
2024-04-22 21:12:48 +1000

    Take huang's search layout and introduce it to dashboard.

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -1,7 +1,23 @@
+import {DashLink} from "@/app/components/DashLink";
+import React from "react";
+
 export default function UserLayout({ children }) {
     return (
-        <div className="flex flex-row gap-0">
-            {children}
+        <div className="container mx-auto h-full p-4">
+            <div className="flex h-full flex-col gap-4 lg:flex-row lg:justify-between">
+                {/* Dash Menu */}
+                <div>
+                    <DashLink
+                        href={"/dashboard/profile"}
+                        header={"Your Details"}
+                        description={"View and update your profile information."}
+                    ></DashLink>
+                </div>
+                {/* Controls Panel */}
+                <div className="h-full grow rounded border p-3">
+                    {children}
+                </div>
+            </div>
         </div>
     );
 }
