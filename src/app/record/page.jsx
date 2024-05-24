55800ed31d374e1047b83b32e8c0d78603643652
lordindex <1074340488@qq.com>
2024-05-24 23:18:47 +0800

    feat: add search

--- src/app/record/page.jsx
+++ src/app/record/page.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const getRecords = async () => {
     try {
-        const res = await fetch(process.env.NEXT_PUBLIC_API_URL, {
+        const res = await fetch(`/api/db/routes`, {
             cache: 'no-store',
         });
 
